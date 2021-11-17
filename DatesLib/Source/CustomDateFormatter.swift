144a585cf5950e16437df0f911aad0c111551dd0
MiguelBS-GH <mikelbra99@gmail.com>
2021-11-17 15:38:37 +0100

    Add customDateFormatter and StringExtension

---
+++ DatesLib/Source/CustomDateFormatter.swift
@@ -0,0 +1,29 @@
+//
+//  DatesClass.swift
+//  DatesLib
+//
+//  Created by Miguel Bragado Sánchez on 17/11/21.
+//
+
+import Foundation
+
+public class CustomDateFormatter {
+    public func format12to24(date date12: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        return dateFormatter.string(from: date12.toDate())
+    }
+
+    public func getDifferenceBetweenDatesInStringDate(from start: Date, to end: Date) -> String {
+        var time = end.timeIntervalSince(start)
+        if start > end { time += 24 * 60 * 60 }
+        return timeIntervalToStringDate(time: time)
+    }
+
+    public func timeIntervalToStringDate(time: TimeInterval) -> String {
+        let horas = time / 60 / 60
+        let minutos = horas.truncatingRemainder(dividingBy: 1) * 60
+        let segundos = minutos.truncatingRemainder(dividingBy: 1) * 60
+        return "\(Int(horas)):\(Int(minutos)):\(Int(round(segundos)))"
+    }
+}
