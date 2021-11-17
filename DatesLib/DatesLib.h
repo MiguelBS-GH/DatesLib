e6a63305c61654117e150baa2417816887b34b74
MiguelBS-GH <mikelbra99@gmail.com>
2021-11-17 11:26:21 +0100

    Initial commit

---
+++ DatesLib/DatesLib.h
@@ -0,0 +1,18 @@
+//
+//  DatesLib.h
+//  DatesLib
+//
+//  Created by Miguel Bragado Sánchez on 17/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DatesLib.
+FOUNDATION_EXPORT double DatesLibVersionNumber;
+
+//! Project version string for DatesLib.
+FOUNDATION_EXPORT const unsigned char DatesLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DatesLib/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/Pods-DatesLib/Pods-DatesLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DatesLib : NSObject
+@end
+@implementation PodsDummy_Pods_DatesLib
+@end
