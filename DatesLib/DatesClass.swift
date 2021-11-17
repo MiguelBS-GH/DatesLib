1b2a9dd120579a3accb6ea1c3c63ead1ad93adfa
MiguelBS-GH <mikelbra99@gmail.com>
2021-11-17 11:32:26 +0100

    Add DatesClass

---
+++ DatesLib/DatesClass.swift
@@ -0,0 +1,14 @@
+//
+//  DatesClass.swift
+//  DatesLib
+//
+//  Created by Miguel Bragado Sánchez on 17/11/21.
+//
+
+import Foundation
+
+class DatesClass {
+    func printDate() {
+        print("01/01/2021")
+    }
+}
